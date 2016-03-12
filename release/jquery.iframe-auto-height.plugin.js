a598b00302a2694a0f7d04c3253cf61b5ac8ab86
jtcdf <bianda132@163.com>
2016-03-12 11:21:06 +0800

    修改ie firefox chrome 显示接近，在浏览器中操作dom变高之后缩不会去的bug

--- release/jquery.iframe-auto-height.plugin.js
+++ release/jquery.iframe-auto-height.plugin.js
@@ -2,59 +2,61 @@
 /*global window, console, jQuery, setTimeout */
 
 /*
-Version: 0.1
+ Version: 0.1
 
-Based on "iframe autoheight jQuery Plugin" code from [https://github.com/house9/jquery-iframe-auto-height]([https://github.com/house9/jquery-iframe-auto-height])
+ Based on "iframe autoheight jQuery Plugin" code from [https://github.com/house9/jquery-iframe-auto-height]([https://github.com/house9/jquery-iframe-auto-height])
 
-The plugin will resize an iframe to the height of its contents
+ The plugin will resize an iframe to the height of its contents
 
-Differences from the original version:
-  added new options:
-      * defaultHeight: integer
-        * Default is 0
-        * Unit is pixels
-        * Sets the iframe height to this value if catch exception (e.g. cross domain policy fail)
-        * Example: `$('iframe').iframeAutoHeight({defaultHeight: 200});`
-    * timeInterval: integer
-      * Default is -1
-      * if timeInterval == -1: resize after iframe loads; if timeInterval > 0: plugin will resized after each timeInterval milliseconds;
-      * Example: `$('iframe').iframeAutoHeight({timeInterval: 500});`
-*/
+ Differences from the original version:
+ added new options:
+ * defaultHeight: integer
+ * Default is 0
+ * Unit is pixels
+ * Sets the iframe height to this value if catch exception (e.g. cross domain policy fail)
+ * Example: `$('iframe').iframeAutoHeight({defaultHeight: 200});`
+ * timeInterval: integer
+ * Default is -1
+ * if timeInterval == -1: resize after iframe loads; if timeInterval > 0: plugin will resized after each timeInterval milliseconds;
+ * Example: `$('iframe').iframeAutoHeight({timeInterval: 500});`
+ */
 
 /*
-Original plugin info:
-  Plugin: iframe autoheight jQuery Plugin
-  Version: 1.6.0
-  Author and Contributors
-  ========================================
-  NATHAN SMITH (http://sonspring.com/)
-  Jesse House (https://github.com/house9)
-  aaron manela (https://github.com/aaronmanela)
-  Hideki Abe (https://github.com/hideki-a)
-  Patrick Clark (https://github.com/hellopat)
-  ChristineP2 (https://github.com/ChristineP2)
-  Mmjavellana (https://github.com/Mmjavellana)
-  yiqing-95 (https://github.com/yiqing-95)
+ Original plugin info:
+ Plugin: iframe autoheight jQuery Plugin
+ Version: 1.6.0
+ Author and Contributors
+ ========================================
+ NATHAN SMITH (http://sonspring.com/)
+ Jesse House (https://github.com/house9)
+ aaron manela (https://github.com/aaronmanela)
+ Hideki Abe (https://github.com/hideki-a)
+ Patrick Clark (https://github.com/hellopat)
+ ChristineP2 (https://github.com/ChristineP2)
+ Mmjavellana (https://github.com/Mmjavellana)
+ yiqing-95 (https://github.com/yiqing-95)
 
-  File: jquery.iframe-auto-height.plugin.js
-  Remarks: original code from http://sonspring.com/journal/jquery-iframe-sizing
-  Description: when the page loads set the height of an iframe based on the height of its contents
-  see README: http://github.com/house9/jquery-iframe-auto-height
+ File: jquery.iframe-auto-height.plugin.js
+ Remarks: original code from http://sonspring.com/journal/jquery-iframe-sizing
+ Description: when the page loads set the height of an iframe based on the height of its contents
+ see README: http://github.com/house9/jquery-iframe-auto-height
 
-*/
+ */
 (function ($) {
   $.fn.iframeAutoHeight = function (spec) {
 
     // set default option values
     var options = $.extend({
-        heightOffset: 0,
-        minHeight: 0,
-        timeInterval: -1,
-        defaultHeight: 0, // used when exception (e.g. cross-domain policy failed)
-        callback: function (newHeight) {},
-        debug: false,
-        diagnostics: false // used for development only
-      }, spec);
+      heightOffset: 10,//预留白高度
+      minHeight: 0,
+      timeInterval: -1,
+      defaultHeight: 0, // used when exception (e.g. cross-domain policy failed)
+      callback: function (newHeight) {
+      },
+      debug: false,
+      ie: navigator.userAgent.indexOf("MSIE") > 0 || !!navigator.userAgent.match(/Trident\/7\./),//判断是否为ie
+      diagnostics: false // used for development only
+    }, spec);
 
     // logging
     function debug(message) {
@@ -95,11 +97,22 @@ Original plugin info:
         }
         try {
           // get the iframe body height and set inline style to that plus a little
-          var $body = $(iframe, window.top.document).contents().find('body');
-          var newHeight = $body[0].scrollHeight + options.heightOffset;
+          //var $body = $(iframe, window.top.document).contents();
+          //var newHeight = $body[0].documentElement.scrollHeight + options.heightOffset;
+          //var $body = $(iframe, window.top.document).contents().find('body');
+          //var newHeight = $body[0].scrollHeight + options.heightOffset;
+          var ieHeightOffset = options.ie ? 20 : 0,
+              outsideRegion = options.ie ? Math.max : Math.min,//外计算
+              insideRegion = options.ie ? Math.min : Math.max;//内计算
+          var D = $(iframe, window.top.document).contents();
+          var newHeight = outsideRegion( //借鉴https://github.com/npr/responsiveiframe/blob/master/src/jquery.responsiveiframe.js代码
+                  insideRegion(D.find('body')[0].scrollHeight, D[0].documentElement.scrollHeight),
+                  insideRegion(D.find('body')[0].offsetHeight, D[0].documentElement.offsetHeight),
+                  insideRegion(D.find('body')[0].clientHeight, D[0].documentElement.clientHeight)
+              ) + options.heightOffset + ieHeightOffset;
 
           if ($.browser.safari) {
-            newHeight = $body[0].offsetHeight + iframe.style.marginBottom + iframe.style.marginTop;
+            newHeight = newHeight + iframe.style.marginBottom + iframe.style.marginTop;
           }
 
           if (newHeight < options.minHeight) {
@@ -108,9 +121,9 @@ Original plugin info:
           }
         }
         catch (ex) {
-          debug("catch exception");
-          debug("set iframe height to defaultHeight");
-          newHeight = options.defaultHeight;
+          //debug("catch exception");
+          //debug("set iframe height to defaultHeight");
+          //newHeight = options.defaultHeight;
         }
 
         if (newHeight != parseInt(iframe.style.height)) {
@@ -127,12 +140,12 @@ Original plugin info:
       }
 
       if (options.timeInterval > 0) {
-          var iframe = this;
-          var delayedResize = function () {
-            resizeHeight(iframe);
-          };
-          debug("set time interval to " + options.timeInterval);
-          setInterval(delayedResize, options.timeInterval);
+        var iframe = this;
+        var delayedResize = function () {
+          resizeHeight(iframe);
+        };
+        debug("set time interval to " + options.timeInterval);
+        setInterval(delayedResize, options.timeInterval);
       }
       else {
         // Check if browser is Opera or Safari (Webkit really, so includes Chrome)
